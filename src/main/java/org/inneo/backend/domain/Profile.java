858a0e6db0c0533ee3cb5ae0fca5a2fca2096c5e
cloundcloundfile <tecnologiaesolucoespr@mail.com>
2024-10-09 23:34:33 -0300

    feat: image profile

--- src/main/java/org/inneo/backend/domain/Profile.java
+++ src/main/java/org/inneo/backend/domain/Profile.java
@@ -1,10 +1,13 @@
 package org.inneo.backend.domain;
 
-import jakarta.validation.constraints.NotBlank;
-import jakarta.persistence.JoinColumn;
-import jakarta.persistence.OneToOne;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
+import jakarta.validation.constraints.NotBlank;
+import org.apache.commons.codec.binary.Base64;
+
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.Transient;
+import jakarta.persistence.OneToOne;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
@@ -12,6 +15,7 @@ import jakarta.persistence.Table;
 
 import lombok.AllArgsConstructor;
 import lombok.NoArgsConstructor;
+import jakarta.persistence.Lob;
 
 import lombok.Builder;
 import lombok.Getter;
@@ -37,8 +41,25 @@ public class Profile extends GenericEntity{
 	@Column(name = "email", unique = true, nullable = false)
 	private String email;
 	
+	@Lob
+	@Column(name = "bits")
+	@JsonProperty(access = JsonProperty.Access.WRITE_ONLY)
+	private byte[] bits;
+	
 	@OneToOne
 	@JoinColumn(name = "usuario_uuid")
 	@JsonProperty(access = JsonProperty.Access.WRITE_ONLY)
 	public Usuario usuario;
+	
+	@Transient
+	private String base64;
+
+	public String getBase64() {
+		if(this.bits == null) return null;
+		return new String(Base64.decodeBase64(this.bits));
+	}
+	
+	public void setBase64(String base64) {
+		this.bits = Base64.encodeBase64(base64.getBytes());
+	}
 }
